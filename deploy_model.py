bc205a356634b869f501a80d30d90b98a63075a4
ArthurHastings <horialitan@yahoo.com>
2025-04-30 09:32:12 +0300

    deploy_model.py modify

--- deploy_model.py
+++ deploy_model.py
@@ -1,20 +1,18 @@
 import subprocess
 import mlflow
 
-# Set the MLFLOW_TRACKING_URI environment variable for the CLI
-# os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
 mlflow.set_tracking_uri("http://localhost:5003")
 
-# Command to serve the model
 command = [
     "mlflow", "models", "serve",
-    "-m", "models:/crypto_sentiment_model_1/1",  # Model URI
-    "--host", "0.0.0.0",  # Host on all interfaces
-    "--port", "5002"  # Port to run the server
+    "-m", "models:/crypto_sentiment_model_1/1",
+    "--host", "0.0.0.0",
+    "--port", "5002",
+    "--env-manager", "local"
 ]
 
-# Run the command
 subprocess.run(command)
+
 
 # TO INSTALL PYENV
 # Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
